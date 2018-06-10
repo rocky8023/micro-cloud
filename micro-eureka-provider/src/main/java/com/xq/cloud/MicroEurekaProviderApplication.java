f8484abbba09bf18b182a3b51e5de74a9c796a7b
zhangpeng32 <zhangpeng32@jd.com>
2018-06-10 22:34:14 +0800

    Initial commit

---
+++ micro-eureka-provider/src/main/java/com/xq/cloud/MicroEurekaProviderApplication.java
@@ -0,0 +1,12 @@
+package com.xq.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEurekaProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEurekaProviderApplication.class, args);
+	}
+}
---
+++ src/main/java/com/xq/cloud/MicroCloudApplication.java
@@ -0,0 +1,12 @@
+package com.xq.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCloudApplication.class, args);
+	}
+}
